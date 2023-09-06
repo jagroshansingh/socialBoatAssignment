877ca96238856342d3e089e733022442f3c7252e
Jagroshan <jagroshansingh615@gmail.com>
2023-09-06 20:28:35 +0530

    card alignment settled and responsiveness added to grid

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import "./css/Card.css"
 
-export const Card = (each) => {
+export const Card = ({each}) => {
+    console.log(each)
   return (
-    <div>
-        <p>{each.each.heading}</p>
-        <video width={'100%'} src={each.each.video}></video>
+    <div id='cardContainer'>
+        <video controls width={'100%'} src={each.video}></video>
+        <h5>{each.heading}</h5>
+        <div id='tags'>
+        {each.tags.map((tag)=><p>#{tag}</p>)}
+        </div>
     </div>
   )
 }
--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import { Card } from './Card'
 import './css/Results.css'
 
-export const Results = (data) => {
-    console.log(data.data)
+export const Results = ({data}) => {
   return (
     <div id='ResultContainer'>
-        {data.data.map((each,i)=><Card key={i} each={each}/>)}
+        {data.map((each,i)=><Card key={i} each={each}/>)}
     </div>
   )
 }
